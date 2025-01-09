b16b290b2177e7c65ff3839a29aa8f78c46cf508
overflowoong <overflowoong@woong-MacBook.local>
2025-01-09 17:39:50 +0900

    FEAT: boolq evaluate modules Update

--- eval/benchmarks/boolq.py
+++ eval/benchmarks/boolq.py
@@ -1,6 +1,53 @@
+import os
+from datetime import datetime
 from datasets import load_dataset
 from evaluate import load as load_metric
 import torch
+from typing import List
+from eval.prompts import PromptManager
+from eval.prompts.templates import get_boolq_answer
+from eval.configs import config
+
+def is_correct(gentext: str):
+    ans_text = get_boolq_answer
+    if ans_text in gentext:
+        prediction_str = gentext.split(ans_text)[-1].strip()
+    else:
+        prediction_str = gentext
+
+    # 간단히 'yes'/'no'가 들어있는지 체크
+    prediction_str_lower = prediction_str.lower()
+    if "yes" in prediction_str_lower and "no" not in prediction_str_lower:
+        return "True"
+    elif "no" in prediction_str_lower and "yes" not in prediction_str_lower:
+        return "False"
+    else:
+        # 애매한 경우, 좀 더 세부 파싱 로직이 필요할 수 있음
+        # 일단 'yes'/'no' 중 하나 선택
+        return "True" if "yes" in prediction_str_lower else "False"
+    
+
+def boolq_res_process(predictions: List, references: List):
+    metric = load_metric("accuracy")
+    accuracy_score = metric.compute(predictions=[1 if ch=='True' else 0 for ch in predictions], references=[1 if ch=='True' else 0 for ch in references])["accuracy"]
+    print(f"BoolQ Accuracy (on {len(predictions)} samples): {accuracy_score:.2f}")
+    os.makedirs(config['log_root_dir'], exist_ok=True)
+
+    # 파일 경로 설정
+    file_path = os.path.join(config['log_root_dir'], "boolq.log")
+    
+    content = f"""BoolQ Accuracy (on {len(predictions)} samples): {accuracy_score:.2f}"""
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    content_with_timestamp = f"[{timestamp}] {content}"
+    
+    # 파일 생성 및 내용 저장
+    with open(file_path, "a") as log_file:
+        log_file.write(content_with_timestamp + "\n")
+    
+    return accuracy_score
+
+
 
 def eval_boolq(model, tokenizer, data_path=None, batch_size=4, device="cuda", split_type="validation"):
     """
@@ -34,9 +81,16 @@ def eval_boolq(model, tokenizer, data_path=None, batch_size=4, device="cuda", sp
         questions = batch["question"]
         contexts = batch["passage"]
         answers = batch["answer"]   # True/False 정답
+        references.extend(list(map(str, answers)))
         
         # boolq 프롬프트 처리
-        prompt = load_prompt("boolq", passage=contexts, question=questions)
+        prompt_manager = PromptManager()
+        prompt = [prompt_manager.get_prompt(
+            benchmark="boolq",
+            prompt_type=config['prompt_type']['boolq'],
+            passage=contexts,
+            question=questions)
+            ]
 
         # 입력 토큰화
         inputs = tokenizer(prompt, padding=True, return_tensors="pt").to(model.device)
@@ -45,20 +99,17 @@ def eval_boolq(model, tokenizer, data_path=None, batch_size=4, device="cuda", sp
         with torch.no_grad():
             output_ids = model.generate(    # shape : (batch_size, sequence_length)
                 **inputs,
-                max_new_tokens=50,
+                max_new_tokens=config['max_new_tokens'],
                 do_sample=False
             )
         
         # 생성된 시퀀스를 토크나이저를 통해 디코딩
         decoded = [tokenizer.decode(ids, skip_special_tokens=True) for ids in output_ids]   # List(batch size)
         
-        
-        
-        
+        # 배치 내의 각 생성된 텍스트에 대해 answer 값 가져오기
+        for gentext in decoded:
+            predictions.append(is_correct(gentext))
 
-        
-
-def boolq_process(split_type="validation"):
-    dataset = load_dataset("boolq", split=split_type)
-    metric = load_metric("accuracy")
+    boolq_accuracy = boolq_res_process(predictions=predictions, references=references)
+    return boolq_accuracy
     
---
+++ eval/prompts/templates/__init__.py
@@ -0,0 +1,1 @@
+from .boolq import get_boolq_templates, get_boolq_answer
