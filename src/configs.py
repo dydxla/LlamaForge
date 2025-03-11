874b63bb1ca83ff136ed0e770a7a6596da4749ec
dydxla <dydxla@gmail.com>
2025-03-12 02:19:28 +0900

    delete trash files

--- src/configs.py
+++
@@ -1,8 +0,0 @@
-conf = {
-    "per_device_train_batch_size": 2,
-    "gradient_accumulation_steps": 2,
-    "learning_rate": 2e-4,
-    "num_train_epochs": 3,
-    "logging_steps": 10,
-    "max_seq_length": 256
-}
